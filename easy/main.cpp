5659dbbb8dceeabc415bdd5d0ae1ad880e5bb2e8
Natsu <649520524@qq.com>
2015-08-28 16:33:42 +0800

    Initial Commit

---
+++ easy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  easy
+//
+//  Created by  HuangHengfeng on 15/8/28.
+//  Copyright (c) 2015年  HuangHengfeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
